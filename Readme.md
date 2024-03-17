19e8daff34722c0f171054861ec79eaa8608cb7e
AND <josh.mukasa@outlook.com>
2024-03-17 12:04:30 -0400

    Created menu and added sql functions. Added readme and sql file

---
+++ Readme.md
@@ -0,0 +1,20 @@
+<h1>COMP 3005 - Assignment 3</h1>
+<hr/>
+<h2>Setup:</h2>
+
+Use the provided setup.sql found in the database_scripts folder to populate the database.
+It will create the students table and add the default student values.
+
+The database most be created manually first in PgAdmin
+
+<hr/>
+<h2>Running:</h2>
+To run this application simply change the global variables at the top of the script with values appropriate for you system
+
+After that you can simply run the python script
+
+You'll be greeted with a menu providing the following 4 option:
+1 - Print all students
+2 - Add a student
+3 - Update a student's email address
+4 - Delete a student
--- main.py
+++ main.py
@@ -1,50 +1,97 @@
 import psycopg
 
-class DatabaseOperations:
+DBNAME = None
+USER = None
+PASSWORD = None
+HOST = None
+PORT = None
 
+
+class DatabaseOperations:
+    # shared connection variable
     conn = None
-    def connect(self):
+
+    # close the connection when this object is destroyed
+    def __del__(self):
+        if self.conn is not None:
+            self.conn.close()
+
+    # Connects to database and stores connection in shared connection variable
+    def connect(self, dbname, user, password, host, port):
         self.conn = psycopg.connect(
-            dbname=,
-            user=,
-            password=,
-            host=,
-            port=s
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
         )
 
-    #TODO: retrieves and displays all records from the students table
     def getAllStudents(self):
         if self.conn is None:
             print("Not connected to database")
             return
 
-        self.conn.execute("SELECT * FROM students")
+        # executes SELECT query on database
+        with self.conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM students")
+            return cursor.fetchall()
 
-    #TODO: inserts a new student records into the students table
     def addStudent(self, first_name, last_name, email, enrollment_date):
+        with self.conn.cursor() as cursor:
+            # executes INSERT statement to add student to Database
+            cursor.execute(
+                "INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES (%s, %s, %s, %s)",
+                (first_name, last_name, email, enrollment_date))
 
-    #TODO: updates the email address for a student with the specified student_id
     def updateStudentEmail(self, student_id, new_email):
+        with self.conn.cursor() as cursor:
+            # executes UPDATE statement to update the email for a given student
+            cursor.execute("UPDATE students SET email = %s WHERE student_id = %s", (new_email, student_id))
 
-    #TODO: deletes the record of the student with the specified student_id
     def deleteStudent(self, student_id):
-    pass
+        with self.conn.cursor() as cursor:
+            # executes DELETE statement to delete a given student
+            cursor.execute("DELETE FROM students WHERE student_id = %s", (student_id,))
+
 
 if __name__ == '__main__':
     databaseOperations = DatabaseOperations()
-    databaseOperations.connect()
+    databaseOperations.connect(DBNAME, USER, PASSWORD, HOST, PORT)
 
-    all_students = databaseOperations.getAllStudents()
-    print(all_students)
+    menu_choice = None
 
-    databaseOperations.updateStudentEmail()
-    all_students = databaseOperations.getAllStudents()
-    print(all_students)
+    while menu_choice != "q":
+        # menu
+        print()
+        print("1 - Print all students")
+        print("2 - Add a student")
+        print("3 - Update a student's email address")
+        print("4 - Delete a student")
+        print("q - Exit")
+        menu_choice = input(">>> ")
 
-    databaseOperations.updateStudentEmail()
-    all_students = databaseOperations.getAllStudents()
-    print(all_students)
+        # calling corresponding function
+        if menu_choice == "1":
+            all_students = databaseOperations.getAllStudents()
+            for row in all_students:
+                print(row)
 
-    databaseOperations.deleteStudent()
-    all_students = databaseOperations.getAllStudents()
-    print(all_students)
+        if menu_choice == "2":
+            databaseOperations.addStudent(
+                input("First Name: "),
+                input("Last Name: "),
+                input("Email: "),
+                input("Enrollment Date (yyyy-mm-dd): ")
+            )
+
+        if menu_choice == "3":
+            databaseOperations.updateStudentEmail(
+                input("Student ID: "),
+                input("New Email: ")
+            )
+
+        if menu_choice == "4":
+            databaseOperations.deleteStudent(
+                input("Student ID: ")
+            )
+
