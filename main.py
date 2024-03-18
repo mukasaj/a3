e2824e19d3092770351c0b086a2bd2e97457424a
AND <josh.mukasa@outlook.com>
2024-03-18 18:33:28 -0400

    Error handling

--- main.py
+++ main.py
@@ -110,6 +110,8 @@ if __name__ == '__main__':
         # calling corresponding function
         if menu_choice == "1":
             all_students = databaseOperations.getAllStudents()
+            if all_students is None:
+                continue
             for row in all_students:
                 print(row)
 
